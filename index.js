1c9aa6ce1fa7d5f736d8436f320107e3b00034a4
nennad <nennadgal@gmail.com>
2015-10-21 15:56:55 +0100

    added disconnect handling neo4j package changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
-var neo4j = require('node-neo4j');
 var util = require('util');
+var neo4j = require('neo4j');
 var EventEmitter = require('events').EventEmitter;
 var generateBinlog = require('./lib/sequence/binlog');
 
@@ -10,17 +10,21 @@ function NeoReplicator(settings, options) {
   EventEmitter.call(this);
 
   // to send table info query
-  var ctrlSettings = cloneObjectSimple(settings.mysql);
-  ctrlSettings.database = 'information_schema';
-  this.ctrlConnectionPool  = mysql.createPool(ctrlSettings);
+  var ctrlDsn = cloneObjectSimple(settings.mysql);
+  ctrlDsn.database = 'information_schema';
+  this.dsn = settings.mysql;
+  this.ctrlDsn = ctrlDsn;
   this.mapping = settings.mapping;
-  //this.ctrlConnection.connect();
-  this.ctrlCallbacks = [];
+  this.interval = null;
+  this.interval2 = null;
+  this.binlogName = null;
+  this.nextPosition = 0;
+  this.listenerAdded = false;
   var neoSettings = settings.neo4j;
-  this.neo4jDb = new neo4j(neoSettings.user+':'+neoSettings.password+'@'+neoSettings.host+':'+neoSettings.port);
+  this.neo4jDb = new neo4j.GraphDatabase('http://'+neoSettings.user+':'+neoSettings.password+'@'+neoSettings.host+':'+neoSettings.port);
+  this._handleDisconnect();
 
-  this.connection = mysql.createConnection(settings.mysql);
-
+  this.ctrlCallbacks = [];
   this.tableMap = {};
   this.ready = false;
   this.useChecksum = false;
@@ -40,12 +44,71 @@ var cloneObjectSimple = function(obj){
 
 util.inherits(NeoReplicator, EventEmitter);
 
+NeoReplicator.prototype._handleDisconnect = function() {
+  var self = this;
+  self.ctrlConnection = mysql.createConnection(self.ctrlDsn);
+
+  self.ctrlConnection.connect(function(err) {
+    if(err) {
+      setTimeout(function() {
+          self._handleDisconnect();
+      }, 5000);
+    } else {
+      if (self.connection && self.connection.state && self.connection.state !== 'disconnected' && self.disconnect()) {
+        self.connection.state = 'disconnected';
+      }
+      self.connection = mysql.createConnection(self.dsn);
+      if (self.ready && self.ctrlConnection.state==='authenticated' && self.connection.state==='disconnected') {
+      //Reconnected
+       self._init();
+       self.start(self.options);
+      }
+      //polling the server to see the connection is active, if error, then the handleDisconnect will kick into action
+      self.interval = setInterval(function(){
+        self.ctrlConnection.query("SELECT 1 AS ctrlConnection", function(err, rows) {
+          if (err) throw err;
+        });
+      }, 30000);
+    }
+  });
+
+  self.ctrlConnection.on('error', function(err) {
+    clearInterval(self.interval);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNREFUSED' || err.code === 'ETIMEDOUT') {
+      self._handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+};
+
+NeoReplicator.prototype._handleConnectionDisconnect = function(){
+  var self = this;
+  if (self.interval2 === null) {
+    var triggerTimeout = (Date.now()/1000|0)+600;
+    self.interval2 = setInterval(function(){
+      //If no activity, Reconnect after every 10mins, to make sure that the connection is alive
+      if (triggerTimeout < Date.now()/1000|0) {
+        self.disconnect()
+      }
+    }, 10000);
+  }
+  if (self.listenerAdded === false) {
+    self.listenerAdded = true;
+    self.on('binlog', function(evt){
+      if (evt.getEventName() !== 'tablemap') {
+        //An event occured, increment the 10min timeout value for reconnection
+        triggerTimeout = (Date.now()/1000|0)+600;
+      }
+    });
+  }
+};
+
 NeoReplicator.prototype._init = function() {
   var self = this;
   var binlogOptions = {
     tableMap: self.tableMap,
   };
-
 
   self.on('binlog', function(evt) {
     evt.replicate(self.mapping);
@@ -102,45 +165,39 @@ NeoReplicator.prototype._isChecksumEnabled = function(next) {
   var ctrlConnection = this.ctrlConnection;
   var connection = this.connection;
 
-  this.ctrlConnectionPool.getConnection(function(err, ctrlConnection) {
-    ctrlConnection.query(sql, function(err, rows) {
-      ctrlConnection.release();
-      if (err) {
-        if(err.toString().match(/ER_UNKNOWN_SYSTEM_VARIABLE/)){
-          // MySQL < 5.6.2 does not support @@GLOBAL.binlog_checksum
-          return next(false);
+  ctrlConnection.query(sql, function(err, rows) {
+    if (err) {
+      if(err.toString().match(/ER_UNKNOWN_SYSTEM_VARIABLE/)){
+        // MySQL < 5.6.2 does not support @@GLOBAL.binlog_checksum
+        return next(false);
+      }
+      throw err;
+    }
+
+    var checksumEnabled = true;
+    if (rows[0].checksum === 'NONE') {
+      checksumEnabled = false;
+    }
+
+    var setChecksumSql = 'set @master_binlog_checksum=@@global.binlog_checksum';
+    if (checksumEnabled) {
+      connection.query(setChecksumSql, function(err) {
+        if (err) {
+          throw err;
         }
-        throw err;
-      }
-
-      var checksumEnabled = true;
-      if (rows[0].checksum === 'NONE') {
-        checksumEnabled = false;
-      }
-
-      var setChecksumSql = 'set @master_binlog_checksum=@@global.binlog_checksum';
-      if (checksumEnabled) {
-        connection.query(setChecksumSql, function(err) {
-          if (err) {
-            throw err;
-          }
-          next(checksumEnabled);
-        });
-      } else {
         next(checksumEnabled);
-      }
-    });
+      });
+    } else {
+      next(checksumEnabled);
+    }
   });
 };
 
 NeoReplicator.prototype._findBinlogEnd = function(next) {
   var self = this;
-  this.ctrlConnectionPool.getConnection(function(err, ctrlConnection) {
-    ctrlConnection.query('SHOW BINARY LOGS', function(err, rows) {
-      ctrlConnection.release();
-      if(err) throw err;
-      next(rows.length > 0 ? rows[rows.length - 1] : null);
-    });
+  self.ctrlConnection.query('SHOW BINARY LOGS', function(err, rows) {
+    if(err) throw err;
+    next(rows.length > 0 ? rows[rows.length - 1] : null);
   });
 };
 
@@ -162,19 +219,16 @@ NeoReplicator.prototype._fetchTableInfo = function(tableMapEvent, next) {
   var sql = util.format(tableInfoQueryTemplate,
     tableMapEvent.schemaName, tableMapEvent.tableName);
 
-  this.ctrlConnectionPool.getConnection(function(err, ctrlConnection) {
-    ctrlConnection.query(sql, function(err, rows) {
-      ctrlConnection.release();
-      if (err) throw err;
+  this.ctrlConnection.query(sql, function(err, rows) {
+    if (err) throw err;
 
-      self.tableMap[tableMapEvent.tableId] = {
-        columnSchemas: rows,
-        parentSchema: tableMapEvent.schemaName,
-        tableName: tableMapEvent.tableName
-      };
+    self.tableMap[tableMapEvent.tableId] = {
+      columnSchemas: rows,
+      parentSchema: tableMapEvent.schemaName,
+      tableName: tableMapEvent.tableName
+    };
 
-      next();
-    });
+    next();
   });
 };
 
@@ -188,10 +242,29 @@ NeoReplicator.prototype.start = function(options) {
 
   var _start = function() {
     self.connection._implyConnect();
+    self._handleConnectionDisconnect();
     self.connection._protocol._enqueue(new self.binlog(function(error, event){
       if(error) return self.emit('error', error);
       // Do not emit events that have been filtered out
       if(event === undefined || event._filtered === true) return;
+
+      //Manage Server Restarts
+      //log gets rotated when mysqld restarts, each new binlogfile starts with position 0
+      // if currently watching binlogName != new binlogName, then it shows server restarted, this is required, because if the script is running and mysqld restarts, the whole events from last file is returned, we need to skip the events that we already processed, we use nextPosition for this purpose.
+      if (event.getTypeName() === 'Rotate') {
+        if (self.binlogName !== event.binlogName) {
+          self.binlogName = event.binlogName;
+          self.nextPosition = 0;
+        }
+      }
+
+      // Skip the processed events
+      if (self.nextPosition >= event.nextPosition) {
+        return;
+      }
+      else {
+        self.nextPosition = event.nextPosition;
+      }
 
       if (event.getTypeName() === 'TableMap') {
         var tableMap = self.tableMap[event.tableId];
@@ -222,20 +295,41 @@ NeoReplicator.prototype.stop = function(){
   var self = this;
   // Binary log connection does not end with destroy()
   self.connection.destroy();
-  self.ctrlConnectionPool.getConnection(function(err, ctrlConnection) {
-    ctrlConnection.query(
-      'KILL ' + self.connection.threadId,
-      function(error, reuslts){
-        ctrlConnection.destroy();
-        self.ctrlConnectionPool.end();
-      }
-    );
-  });
+  self.ctrlConnection.query(
+    'KILL ' + self.connection.threadId,
+    function(error, rows){
+      self.ctrlConnection.destroy();
+    }
+  );
+};
+
+NeoReplicator.prototype.disconnect = function(){
+  var self = this;
+  // Binary log connection does not end with destroy()
+  self.connection.destroy();
+  self.ctrlConnection.query(
+    'KILL '+self.connection.threadId,
+    function(err, rows){
+      //KILLED MySQL self.connection.threadId
+      self.reconnect();
+    }
+  );
+};
+
+NeoReplicator.prototype.reconnect = function(){
+  var self =  this;
+  clearInterval(self.interval2);
+  self.interval2 = null;
+  self.connection = mysql.createConnection(self.dsn);
+  self.start(self.options);
 };
 
 NeoReplicator.prototype._skipEvent = function(eventName){
   var include = ['tablemap', 'writerows', 'updaterows', 'deleterows'];
-  var exclude = [];
+  var exclude = this.options.excludeEvents;
+  if (Array.isArray(include) && include.indexOf('rotate') === -1) {
+    include.push('rotate');
+  }
   return !(
    (include === undefined ||
     (include instanceof Array && include.indexOf(eventName) !== -1)) &&
